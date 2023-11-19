e7e0367fb23d09b83559e231da04c9b959bb467d
leon-kxng <145435067+leon-kxng@users.noreply.github.com>
2023-11-19 22:43:55 +0300

    finishing assignment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 import './App.css';
 import Table from './components/Table';
 import Form from './components/form';
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,87 +1,26 @@
-import React from "react";
-import { useState } from "react";
 
-const Form = ({className,removeTransaction}) => {
+import { useState } from "react"
 
-
-    let [category,setCategory] = useState(null)
-    let [description,setDescription] = useState(null)
-    let [amount,setAmount] = useState(null)
-    let [date,setDate] = useState(null)
-
-    let handleDescription = (value) => {
-        setDescription(value)
-        console.log(value)
-    }
-    let handleDate= (value) => {
-        setDate(value)
-        console.log(value)
-    }
-    let handleAmount = (value) => {
-        setAmount(value)
-        console.log(value)
-    }
-    let handleCategory = (value) => {
-        setCategory(value)
-        console.log(value)
-    }
- 
-    let newObj = {
-        date,
-        description,
-        category,
-        amount
+function Searchbar({handleSearch,handleBack,appendTransaction}) {
+    let [searchValue,setSearchValue] = useState('')
+    
+    let handleSetState = (value) => {
+        setSearchValue(value)
+        console.log(searchValue)
     }
 
-    let handleSubmit = (e) =>{
-        e.preventDefault()
-        console.log("active post")
-        if(newObj.date !== null && newObj.description !== null && newObj.category !== null && newObj.amount !== null){
-        fetch("https://my-json-server.typicode.com/leon-kxng/Bank-of-Flatiron/transactions",{
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-            body: JSON.stringify(newObj)
-        })
-        .then((response) =>{ console.log(response)})
-    }
-    else{
-        console.log("error")
-    }
-
-}
-
-
+        
     return ( 
-        <form className={className}>
-            <button onClick = {removeTransaction} id="remove-transaction">X</button>
-        <select onClick = {(e)=>{
-            handleCategory(e.target.value)
-        }} id="categories">
-            <option>Food</option>
-            <option>Fashion</option>
-            <option>Gift</option>
-            <option>Transportation</option>
-            <option>Food</option>
-         </select>
-         <br/>
-           <label for = "textarea">Description</label>
-           <br/>
-           <textarea onChange = {(e)=>{handleDescription(e.target.value)}} value = {description}></textarea>
-           <br/>
-           <label for = "amountInput">Amount </label>
-           <br/>
-           <input onChange = {(e)=>{handleAmount(parseInt(e.target.value))}} value = {amount} type="number" id="amountInput"></input>
-           <br/>
-           <label for = "amountInput">Date</label>
-           <br/>
-           <input onChange = {(e)=>{handleDate(e.target.value)}} value = {date} type="date" id="dateInput"></input>
-           <br/>
-           <button onClick={handleSubmit} id = "submit-button" type="submit">submit</button>
-        </form>
+     <div>
+        <h1 id="pageHeader">Bank of Flatiron</h1>
+        <a href ="https://bank-of-flatiron3.vercel.app/" id='back-button' >back</a>
+        <input onChange = {(e)=>{handleSetState(e.target.value)}} value ={searchValue} id='searchInput' type = 'search' placeholder = 'search'></input>
+        <button onClick={() => {
+          handleSearch(searchValue)
+          }} id = "search-button">Search</button>  
+          <button onClick = {appendTransaction} id='add-button'>Add Transaction</button>
+     </div>
      );
 }
- 
-export default Form;
+
+export default Searchbar;
