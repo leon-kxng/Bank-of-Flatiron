7edaed714db82822794b22af25f66c0582bc1987
leon-kxng <145435067+leon-kxng@users.noreply.github.com>
2023-11-19 18:53:31 +0300

    completing assignment

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+//function to display the name of the bank
+function Header(){
+    return(
+    <div>
+    <header className="App-header">
+     <h1>Flatiron Bank </h1>
+    </header>
+
+    </div>
+    )
+}
+export default Header;
